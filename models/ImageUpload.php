c9320f272dee553767a3ec787315c23508e80e16
Stas <stastolsto@gmail.com>
2020-07-05 21:20:44 +0300

    Work with Image

---
+++ models/ImageUpload.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+use yii\web\UploadedFile;
+
+class ImageUpload extends Model{
+
+    public $image;
+
+    public function rules()
+    {
+        return [
+            [['image'], 'required' ],
+            [['image'], 'file', 'extensions' => 'jpg,png']
+        ];
+    }
+
+    public  function uploadFile(UploadedFile $file, $currentImage)
+    {
+        $this->image = $file;
+
+        if($this->validate()) {
+
+            $this->deleteCurrentImage($currentImage);
+
+            $filename = $this->generateFilename();
+
+            $file->saveAs($this->getFolder() . $filename);
+
+            return $filename;
+        }
+    }
+
+    public function getFolder()
+    {
+        return Yii::getAlias('@web') . 'uploads/';
+    }
+
+    public function generateFilename()
+    {
+        return strtolower(md5(uniqid($this->image->baseName)) . '.' . $this->image->extension);
+    }
+
+    public function deleteCurrentImage($currentImage)
+    {
+        if (file_exists($this->getFolder() . $currentImage) &&
+            is_file($this->getFolder() . $currentImage)) {
+            unlink($this->getFolder() . $currentImage);
+        }
+    }
+}
